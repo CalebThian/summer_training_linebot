c871dc4c9a654295d36a9142316d01500ca2823c
CalebThian <calebthian@gmail.com>
2021-07-06 16:31:19 +0800

    accelerate rollcall speed

--- google_sheet_api.py
+++ google_sheet_api.py
@@ -35,61 +35,7 @@ def open_google_sheet(target_gs = "https://docs.google.com/spreadsheets/d/1ohd8Y
     sht = gc.open_by_url(target_gs)
     return sht
 
-# 點名
-def attend(name,message,sht):
-    wks = sht.worksheets()
-    col_list = list(string.ascii_uppercase)
-    item_row = 2
-    
-    # 找到姓名的cell，假設每一個worksheet都是一樣的位置
-    # 假設從第二頁開始查
-    # 因爲已知都在‘B’，所以爲了節省時間就不搜尋了
-    name_col = 'B'
-    '''
-    wks_cur = wks[1]
-    for alpha in col_list:
-        cell = alpha+item_row
-        if wks_cur.cell(cell).value == "姓名":
-            name_col = alpha
-            break
-    '''
-    
-    # 找對應格子， 設爲True
-    # 因爲信息對應的column也是已知，所以這裏用dict記住就可以了
-    message_col = dict()
-    mes_list = wks[1].range('E2:P2')[0]
-    for i in range(12):
-        message_col[mes_list[i].value] = col_list[4+i]
-
-    
-    # 點名
-    for i in range(1,len(wks)):
-        wks_cur = wks[i]
-        ite = 3
-        while True:
-            print("\r"+wks_cur.title+"--"+name_col + str(ite),end = "")
-            if wks_cur.cell(name_col + str(ite)).value == name:
-                wks_cur.update_value(message_col[message] + str(ite),'True')
-                return 1
-            elif wks_cur.cell(name_col + str(ite)).value == "":
-                break
-            else:
-                ite = ite+1
-
-    # Failed to find the name
-    return 0
-
-def rollcall(name_list,message,sht):
-    success = []
-    fail = []
-    for name in name_list.split():
-        print(name)
-        if attend(name,message,sht):
-            success.append(name)
-        else:
-            fail.append(name)
-    return success,fail
-
+# 確認每個名字與其對應的點名格子編碼
 def create_dict(message,sht):
     d_list = []
     
@@ -116,13 +62,71 @@ def create_dict(message,sht):
     
     return d_list
 
+def get_cell(target,d_list):
+
+    # Check 100% accuracy
+    for i in range(len(d_list)):
+        keys = d_list[i].keys()
+        for key in keys:
+            if target == key:
+                return i,d_list[i][key]
+
+    # Check >50% accuracy        
+    for i in range(len(d_list)):
+        keys = d_list[i].keys()
+        for key in keys:
+            hit = 0.0
+            for k in key:
+                if k in target:
+                    hit = hit + 1.    
+            acc = hit/len(key)
+            if acc >= 0.5:
+                return i,d_list[i][key]
+            
+    # If not exists
+    return -1,"None"
+
+# 點名
+def attend(tick,sht):
+    wks = sht.worksheets()
+    # 點名
+    for i in range(len(wks)-1):
+        wks_cur = wks[i+1]
+        for t in tick[i]:
+            wks_cur.update_value(t,'True')
+        
+    # Failed to find the name
+    return 0
+
+
+
+def rollcall(name_list,message,sht):
+    success = []
+    tick = [[],[],[],[],[],[]]
+    fail = []
+    d = create_dict(message,sht)
+    for name in name_list.split():
+        print(name)
+        i,cell = get_cell(name,d)
+        if i!=-1:
+            success.append(name)
+            tick[i].append(cell)
+        else:
+            fail.append(name)
+    
+    attend(tick,sht)
+    
+    return success,fail
+
+
+
+              
 
 if __name__ == '__main__':
     #A1 = sht[1].cell('E51')
     #sht[1].update_value('E51','True')
     sht = open_google_sheet('https://docs.google.com/spreadsheets/d/1ohd8YRgh9ghewZaSP39W0dhPyKw_xI0kbWu_SoClw3A/edit#gid=680064596') 
-    wks = sht[2]
-    print(wks.get_values_batch(['B3:B'])[0][2:])
     d = create_dict("信息一",sht)
-    print(d)
-    #rollcall('静音\n謝亞城 田家樂 高苡程\n虛俊翰 恩慈高\n黃柏\n陳孜安 其恩\n黃凡芸 其恩路\n張晴雯 曾業偉\n王宏惠\nHsin 致美張\nChunyi\n得真','信息一',sht)
+    target = ""
+    #i,cell = get_cell(target,d)
+    s,c = rollcall('静音\n謝亞城 田家樂 高苡程\n虛俊翰 恩慈高\n黃柏\n陳孜安 其恩\n黃凡芸 其恩路\n張晴雯 曾業偉\n王宏惠\nHsin 致美張\nChunyi\n得真','信息一',sht)
